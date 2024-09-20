45fb1b41b1ba63d5a8fed9db9dd6cadd21b48d41
vysnoo <vishnukumaresan21@gmail.com>
2024-09-20 20:25:47 +0530

    12 Hours format

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function setTime() {
   const date = new Date();
-  document.querySelector("#hour").textContent = date
-    .getHours()
+  let hours = date.getHours();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  document.querySelector("#hour").textContent = hours
     .toString()
     .padStart(2, "0");
   document.querySelector("#minute").textContent = date
@@ -12,6 +15,7 @@ function setTime() {
     .getSeconds()
     .toString()
     .padStart(2, "0");
+  document.querySelector("#ampm").textContent = ampm;
 }
 
 setTime();
